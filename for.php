2f67298a4ca65e6f6bd5f8097254be09410a17ed
fatkulumar <fatkulumar@gmail.com>
2019-03-08 15:01:37 +0700

    update for
    nambah option

--- for.php
+++ for.php
@@ -3,19 +3,27 @@
     if (mysqli_connect_errno()) {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
-    $result = mysqli_query($con, "SELECT t1.nama, t2.nama
-                        FROM data t1
-                        INNER JOIN data t2
-                        ON t2.id > t1.id");
+    $result = mysqli_query($con, "SELECT t1.nama_kriteria, t2.nama_kriteria
+                        FROM tb_kriteria t1
+                        INNER JOIN tb_kriteria t2
+                        ON t2.id_kriteria > t1.id_kriteria");
     echo "<table>";
     $no = 0;
     while($row = mysqli_fetch_array($result)) {
         echo "<tr>";
-        echo "<td>". ++$no . "</td>"; 
+        echo "<td>". ++$no . "</td>";
+        
             for($i = 0; $i < count($row)-1; $i++){
                 $keys = array_keys($row);
+                if($i == 1){
+                    echo "<td>";
+                    echo '<select name="" id="">
+                            <option value=""></option>
+                        </select>';
+                    echo "</td>";
+                } 
                     echo "<td>" . $row[$keys[$i]] . "</td>";
+            }
+                echo "</tr>";
     }
-        echo "</tr>";
-    }
-        echo "</table>";
+    echo "</table>";
